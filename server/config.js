9fe8d107fc0af4d101d1d62251868c41cd8fcef7
Michael Pollind <polli104@mail.chapman.edu>
2015-04-13 09:36:53 -0700

    fix to gitignore

--- server/config.js
+++
@@ -1,9 +0,0 @@
-var config = {
- database_host: "localhost",
- database_name: "Food",
- database_password : "awefaweqv83hv9283bpq",
- url : "http:\\localhost:3000",
- port : 3000
-}
-
-module.exports = config;
