51a24cde425655d425fda507947ca2159ab87d78
Michael Pollind <polli104@mail.chapman.edu>
2015-04-21 00:13:57 -0700

    work on draggable

--- app_2/www/js/foodSuggestionController.js
+++ app_2/www/js/foodSuggestionController.js
@@ -1,13 +1,27 @@
 app.controller("foodSuggestionController", function($window, $http, $scope, $stateParams, $location) {
-    $scope.foods = [];
+    $scope.foods = {};
 
+    var pecboard_food = JSON.parse($window.localStorage.getItem("food"));
 
     $http.get(URL + 'rest/food?mealId=' + $stateParams.meal + '&featureId=' + $stateParams.feature, {}).
     success(function(data, status, headers, config) {
         console.log(data);
-        $scope.foods = data.data;
-    }).
-    error(function(data, status, headers, config) {
+        for (var i = 0; i < data.data.length; i++) {
+            if (!pecboard_food.hasOwnProperty("food-" + data.data[i].id)) {
+                $scope.foods["food-" + data.data[i].id] = {
+                    entry: data.data[i],
+                    pecboard: true
+                };
+            } else {
+                $scope.foods["food-" + data.data[i].id] = {
+                    entry: data.data[i],
+                    pecboard: false
+                };
+            }
+        };
+
+        //$scope.foods = data.data;
+    }).error(function(data, status, headers, config) {
 
     });
 
@@ -15,11 +29,22 @@ app.controller("foodSuggestionController", function($window, $http, $scope, $sta
     $scope.viewFoodItem = function(id) {
         $location.url("/food/" + id);
     }
-    $scope.togglePecBoard = function(food) {
+    $scope.togglePecBoard = function(key, food) {
+        var data = $window.localStorage.getItem("food");
+        var out;
+        try {
+            out = JSON.parse(data);
 
-        if (food.pecboard)
-            food.pecboard = false;
-        else
-            food.pecboard = true;
+            if (food.pecboard) {
+                out[key] = food.entry;
+                food.pecboard = false;
+            } else {
+                delete out[key];
+                food.pecboard = true;
+            }
+        } catch (e) {
+            out = {};
+        }
+        $window.localStorage.setItem("food", JSON.stringify(out));
     }
 });
--- app_2/www/js/pecBoardController.js
+++ app_2/www/js/pecBoardController.js
@@ -1,3 +1,10 @@
 app.controller("pecBoardController", function($window, $scope, $stateParams, $location) {
-    // $scope.
+    var pecboard_food = JSON.parse($window.localStorage.getItem("food"));
+    $scope.pecBoardItems = pecboard_food;
+
+    $scope.onDropComplete = function(data, evt) {
+        console.log("drag success, data:", evt);
+        $scope.pecBoardItems[data].x = evt.x;
+        $scope.pecBoardItems[data].y = evt.y;
+    }
 });
